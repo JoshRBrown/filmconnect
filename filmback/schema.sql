2cad84c64a478c6e976170cfbbd175636b36d98f
Stephen <smjarr89@gmail.com>
2018-07-30 12:51:24 -0400

    added backend setup

---
+++ filmback/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+  user_id INT PRIMARY KEY,
+  user_name varchar(50) NOT NULL
+);
+
+CREATE TABLE reviews (
+  user_id int NOT NULL REFERENCES users(user_id),
+  movie_id int NOT NULL REFERENCES movies(movie_id),
+  rating int NOT NULL,
+  comment varchar(250)
+);
+
+CREATE TABLE movies (
+  movie_id int PRIMARY KEY,
+  title varchar NOT NULL,
+  overview varchar,
+  release_date varchar,
+  poster varchar
+);
