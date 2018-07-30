c2184b6787a741b4e35323092ee144b4194b04f4
Stephen <smjarr89@gmail.com>
2018-07-30 16:11:54 -0400

    added function to add user to database upon login if they arent a user now

--- filmback/db.js
+++ filmback/db.js
@@ -11,22 +11,36 @@ const db = pgp(cn);
 
 
 function insertMovie(id, title, overview, release_date, poster) {
-  return db.one('INSER INTO movies (movied_id, title, overview, release_date, poster) VALUES ($1, $2, $3, $4, $5);'[id, title, overview, release_date, poster]);
+  return db.one('INSER INTO movies (movied_id, title, overview, release_date, poster) VALUES ($1, $2, $3, $4, $5);', [id, title, overview, release_date, poster]);
 };
 
 function insertUser(id, username) {
-  return db.one('INSERT INTO users (user_id, user_name) VALUES ($1, $2);'[id, username]);
+  return db.one(`INSERT INTO users (user_id, user_name) VALUES ('$1#', '$2#') returning user_id;`, [id, username]);
 };
 
 function insertReview(user_id, movie_id, rating, comment) {
-  return db.one('INSERT INTO reviews (user_id, movie_id, rating, comment) VALUES ($1, $2, $3, $4);'[user_id, movie_id, rating, comment]);
+  return db.one('INSERT INTO reviews (user_id, movie_id, rating, comment) VALUES ($1, $2, $3, $4);', [user_id, movie_id, rating, comment]);
 };
 
 function getMovieReviews(movie_id) {
-  return db.any('SELECT * FROM reviews WHERE movie_id = $1;' [movie_id]);
+  return db.any('SELECT * FROM reviews WHERE movie_id = $1;', [movie_id]);
 };
 
 function getUserReviews(user_id) {
-  return db.any('SELECT * FROM reviews WHERE user_id = $1;'[user_id]);
+  return db.any('SELECT * FROM reviews WHERE user_id = 1#', [user_id]);
 };
+
+function getUser(user_id) {
+  return db.oneOrNone('SELECT * from users WHERE user_id = $1;', [user_id]);
+};
+
+module.exports = {
+  insertMovie,
+  insertUser,
+  insertReview,
+  getMovieReviews,
+  getUserReviews,
+  getUser
+};
+
 
