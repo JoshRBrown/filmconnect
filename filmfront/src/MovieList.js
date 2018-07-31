c1b1a92498e5118aa899506a4745b19de19a5e9c
Josh Brown <jbrown016@gmail.com>
2018-07-31 11:40:25 -0400

    randomint function for random api call

--- filmfront/src/MovieList.js
+++ filmfront/src/MovieList.js
@@ -11,8 +11,9 @@ class MovieList extends React.Component {
   }
 
   componentDidMount(){
+    let randomInt = 
     console.log(this.props.match.params)    
-      fetch('https://api.themoviedb.org/3/discover/movie?api_key=644f21e59b17cbf67e64dbcba7a57278&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+      fetch(`https://api.themoviedb.org/3/discover/movie?api_key=644f21e59b17cbf67e64dbcba7a57278&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${}`)
       .then(response => response.json())
       .then(data => (data.results))
       .then(arr => {
