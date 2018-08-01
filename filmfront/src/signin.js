a1496aca1fb4964b809513ba2c9bc59fd3db8492
Stephen <smjarr89@gmail.com>
2018-08-01 12:35:21 -0400

    fixed auth and DB review entry issue

--- filmfront/src/signin.js
+++ filmfront/src/signin.js
@@ -5,10 +5,9 @@ import {Link} from 'react-router-dom';
 const Signin = (props) => {
   return(
   <div className='home-page'>
-    <a href="http://localhost:4000/login"><button>Sign In</button></a>
+    <a href="/login"><button>Sign In</button></a>
   </div>
   )
 };
-
 
 export default Signin;
